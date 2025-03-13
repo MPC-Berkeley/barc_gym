65b6cdcf8397c99004b93b9f107b631d425aef08
Shengfan Cao <csf990123@gmail.com>
2025-03-13 16:42:25 -0700

    Updated dependencies

--- mpclab_controllers/setup.py
+++ mpclab_controllers/setup.py
@@ -4,10 +4,12 @@ setup(
     name='mpclab_controllers',
     version='0.1',
     packages=['mpclab_controllers'],
-    install_requires=['numpy', 
-                      'scipy',
-                      'casadi',
-                      'cvxpy',
-                      'matplotlib',
-                      'julia']
+    install_requires=[
+        # 'numpy',
+        # 'scipy',
+        # 'casadi',
+        # 'cvxpy',
+        # 'matplotlib',
+        # 'julia'
+    ]
 )
